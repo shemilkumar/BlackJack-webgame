455c411032999e10e92f6b021c1e0b68c4065d65
shemilkumar <ekaravindhakshan@gmail.com>
2022-06-06 20:10:02 +0530

    Project working in console

--- src/js/script.js
+++ src/js/script.js
@@ -5,36 +5,17 @@ const playercardsContainer = document.querySelector('.cards-container-player');
 const sumDealer = document.querySelector('.dealer-sum-amount');
 const sumPlayer = document.querySelector('.player-sum-amount');
 
+
 const btnStay = document.querySelector('.btn-stay');
 const btnHit = document.querySelector('.btn-hit');
 
-// const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
-// // const ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'queen', 'king', 'ace'];
+let deck, player, dealer, chips, bet, choice, round = 0;
 
-// const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
-
-// const values = {
-//     '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'ten': 10, 'jack': 10,
-//     'queen': 10, 'king': 10, 'ace': 11,
-// };
-
-// const createCard = (suitsValue, ranksValue) => ({ suit: suitsValue, rank: ranksValue });
-
-// // const cards = ranks.map(ranksValue => suits.forEach(suitsValue => createCard(suitsValue, ranksValue)));
-
-// const allCards = [];
-
-// for (const suit of suits) {
-//     for (const rank of ranks) {
-//         allCards.push(createCard(suit, rank));
-//     }
-// }
 const cardValues = {
     '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'jack': 10,
     'queen': 10, 'king': 10, 'ace': 11,
 };
 
-let deck, player, dealer;
 
 class Deck {
 
@@ -71,8 +52,25 @@ class Deck {
 }
 
 class Hand {
+
     handCards = [];
     handValue = 0;
+
+    constructor() {
+        // this._hitEvent();
+        // this._stayEvent();
+        // debugger;
+    }
+
+    // _hitEvent() {
+    //     this.btnHit.addEventListener('click', this.hitClicked.bind(this));
+    //     // this.btnHit.addEventListener('click', function () {
+    //     //     console.log("clicked");
+    //     // })
+    // }
+    // _stayEvent() {
+    //     this.btnStay.addEventListener('click', this.stayClicked.bind(this));
+    // }
 
     addCard(card) {
         this.handCards.push(card);
@@ -87,44 +85,106 @@ class Hand {
     stay(card) {
         while (this.handValue < 17)
             this.addCard(card);
+
     }
 
-    // _hitOrStay(card, value) {
-    //     if (value === 'hit') {
-    //         this.addCard(card);
-    //     }
-    //     if (value === 'stay') {
-    //         while (this.handValue < 17)
-    //             this.addCard(card);
-    //     }
+    // hitClicked() {
+    //     console.log(this);
+    //     this.hit(deck.deal());
+    //     hitCheck();
+    //     this.btnHit.removeEventListener('click', this.stayClicked.bind(this));
+    // }
+
+    // stayClicked() {
+    //     console.log("yes");
+    //     this.stay(deck.deal());
+    //     stayCheck();
     // }
 
 }
 
-// class Chips {
+class Chips {
+    chips = 100;
 
-//     // givivng 100 chips to the user
-//     chips = 100;
+    winBet(bet) {
+        this.chips += bet;
+    }
 
-//     // when win the bet, add the bet amount to the chips
-//     winBet(bet) {
-//         this.chips += bet;
-//     }
+    looseBet(bet) {
+        this.chips -= bet;
+    }
 
-//     // when loose the bet, decrease the bet amount from the chips
-//     looseBet() {
-//         this.chips -= bet;
-//     }
-// }
+    takeBet() {
+        while (true) {
+            // alert("You have 100 chips in your pocket");
+            const betFromUser = +(prompt("Place your bet"));
 
+            if (Number.isFinite(betFromUser)) {
+
+                if (betFromUser > 0) {
+                    if (betFromUser <= this.chips) {
+                        return betFromUser;
+                    }
+                    else {
+                        alert("your bet amount is more than your available chips");
+
+                    }
+                }
+                else {
+                    alert("Please enter valid amount");
+                }
+
+            }
+            else {
+                alert("Please enter a number");
+            }
+        }
+    }
+}
+
+const dealerWin = function (chips, bet, dealer, player) {
+
+    chips.looseBet(bet);
+    console.log(chips.chips);
+
+    console.log("dealer wins --");
+    console.log("Dealer");
+    console.log(dealer.handCards, dealer.handValue);
+    console.log("Player");
+    console.log(player.handCards, player.handValue);
+}
+
+const playerWin = function (chips, bet, dealer, player) {
+
+    chips.winBet(bet);
+    console.log(chips.chips);
+
+    console.log("player wins --");
+    console.log("Dealer");
+    console.log(dealer.handCards, dealer.handValue);
+    console.log("Player");
+    console.log(player.handCards, player.handValue);
+}
+
+const playAgain = function () {
+    choice = prompt("Do u wanna play again ? (y/n)");
+    if (choice === 'y') call();
+}
 
 const init = function () {
+    chips = new Chips();
+    // bet = chips.takeBet();
+}
+
+const play = function () {
+    console.log("=============== round " + (++round) + "================")
 
     deck = new Deck();
 
     player = new Hand();
     dealer = new Hand();
 
+    console.log(bet);
 
     const players = ["player", "dealer"];
 
@@ -139,41 +199,32 @@ const init = function () {
     console.log(player.handCards, player.handValue);
 
     if (player.handValue === 21) {
-        console.log("player wins -- player = 21");
-        console.log("Dealer");
-        console.log(dealer.handCards, dealer.handValue);
-        console.log("Player");
-        console.log(player.handCards, player.handValue);
+
+        playerWin(chips, bet, dealer, player);
+        playAgain();
+
     }
-
-    btnHit.addEventListener('click', function () {
-        player.hit(deck.deal());
-        hitCheck();
-    });
-
-    btnStay.addEventListener('click', function () {
-        dealer.stay(deck.deal());
-        stayCheck();
-    });
 }
 
 const hitCheck = function (dealerCheck = false) {
     if (!dealerCheck) {
-        if (player.handValue > 22) {
-            console.log("dealer wins -- player > 21");
-            console.log("Dealer");
-            console.log(dealer.handCards, dealer.handValue);
-            console.log("Player");
-            console.log(player.handCards, player.handValue);
+        if (player.handValue >= 22) {
+
+            dealerWin(chips, bet, dealer, player);
+            playAgain();
+
         }
     }
     if (dealerCheck) {
-        if (dealer.handValue > 22) {
-            console.log("player wins -- dealer > 21");
-            console.log("Dealer");
-            console.log(dealer.handCards, dealer.handValue);
-            console.log("Player");
-            console.log(player.handCards, player.handValue);
+        if (dealer.handValue >= 22) {
+
+            playerWin(chips, bet, dealer, player);
+
+            playAgain();
+        }
+        else {
+            dealerWin(chips, bet, dealer, player);
+            playAgain();
         }
     }
 }
@@ -182,41 +233,59 @@ const stayCheck = function () {
 
     if (dealer.handValue < 22) {
         if (dealer.handValue > player.handValue) {
-            console.log("dealer wins,, greater than player");
-            console.log("Dealer");
-            console.log(dealer.handCards, dealer.handValue);
-            console.log("Player");
-            console.log(player.handCards, player.handValue);
+
+            dealerWin(chips, bet, dealer, player);
+            playAgain();
         }
         else {
-
-            dealer.hit(deck.deal());
-            hitCheck(true);
-            // console.log("player wins,, greater than dealer");
-            // console.log("Dealer");
-            // console.log(dealer.handCards, dealer.handValue);
-            // console.log("Player");
-            // console.log(player.handCards, player.handValue);
+            while (dealer.handValue <= player.handValue) {
+                dealer.hit(deck.deal());
+                hitCheck(true);
+            }
         }
     }
     else {
-        console.log("player wins -- dealer > 21");
-        console.log("Dealer");
-        console.log(dealer.handCards, dealer.handValue);
-        console.log("Player");
-        console.log(player.handCards, player.handValue);
+        playerWin(chips, bet, dealer, player);
+        playAgain();
     }
 
 }
 
+btnHit.addEventListener('click', function () {
+    player.hit(deck.deal());
+    hitCheck();
+});
+
+btnStay.addEventListener('click', function () {
+    console.log("yes");
+    dealer.stay(deck.deal());
+    stayCheck();
+});
+
+const call = function () {
+
+    if (round === 0) {
+        init();
+        bet = chips.takeBet();
+        play();
+        console.log(round);
+    } else {
+        if (chips.chips > 0) {
+            bet = chips.takeBet();
+            play();
+            console.log("yes in if");
+        }
+        else {
+            console.log("You loose the game !");
+            alert("You loose the game, you dont have any chips for bet !!!");
+            return;
+        }
+    }
+}
+
+
 init();
-// startPlay();
-
-
-
-
-
-
+call();
 
 
 
